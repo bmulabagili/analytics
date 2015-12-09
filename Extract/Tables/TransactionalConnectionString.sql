2cd85a0ab81f1924d89365c31889e0e613c7891f
shannonlowder <slowder@52projectsinc.com>
2015-12-09 15:39:37 -0500

    More ETL changes.Load packages are not working yet.

---
+++ Extract/Tables/TransactionalConnectionString.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [Extract].[TransactionalConnectionString]
+(
+    --Source Specific Columns
+      ConnectionStringID INT 
+    , [Source] NVARCHAR(1000)
+    , Note NVARCHAR(1000) 
+
+    --ETL Specific Columns
+    , ExecutionID VARCHAR (50)   NOT NULL
+    --, ETLActionID INT NOT NULL -- not needed
+    , InsertedDateTime DATETIME DEFAULT(GETUTCDATE())
+    --, UpdatedDateTime DATETIME DEFAULT(GETUTCDATE()) -- not needed
+    --, Hashvalue VNVARCHAR(64) NOT NULL --not needed
+)
+GO
+CREATE CLUSTERED INDEX CIX_ExtractTransactionalConnectionString__ConnectionStringID
+    ON [Extract].[TransactionalConnectionString](ConnectionStringID)
---
+++ Extract/Tables/TransactionalTenant.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [Extract].[TransactionalTenant]
+(
+    --Source Specific Columns
+	 TenantID INT
+    , TenantName NVARCHAR(255)
+    , ConnectionStringID INT
+    , isActive INT
+
+    --ETL Specific Columns
+    ,ExecutionID VARCHAR (50)   NOT NULL
+    --, ETLActionID INT NOT NULL -- not needed
+    , InsertedDateTime DATETIME DEFAULT(GETUTCDATE())
+    --, UpdatedDateTime DATETIME DEFAULT(GETUTCDATE()) -- not needed
+    --, Hashvalue VNVARCHAR(64) NOT NULL --not needed
+
+)
+GO
+CREATE INDEX CIX_ExtractTransactionalTenant__TenantID
+    ON [Extract].[TransactionalTenant](TenantID)
+
