d292ab5a4f3fda4144333827525a775a2eac95ae
shannonlowder <slowder@52projectsinc.com>
2015-12-22 15:38:51 -0500

    updated the powershell script

---
+++ SetupTenantETLFolders.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module SQLPS -DisableNameChecking
+
+
+$templateFolderLocation = "E:\ETLContainer\TenantID\*";
+$destinationFolderRoot = "E:\ETLContainer";
+
+$Results = Invoke-SQLCmd  -Serverinstance localhost -database Analytics -query "SELECT DISTINCT TenantID FROM Tenant";
+
+Foreach($result in $results) {
+    
+    $destinationFolder = $destinationFolderRoot + "\" + $result.TenantID;
+
+    Copy-Item $templateFolderLocation $destinationFolder -recurse -ErrorAction SilentlyContinue 
+}
