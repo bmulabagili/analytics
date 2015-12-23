ef216457dbbf4e4a37aa2d03ad8c0633a139ee39
shannonlowder <slowder@52projectsinc.com>
2015-12-22 22:20:52 -0500

    synced the project back to dev schema

---
+++ DW/Tables/DimCampus.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [DW].[DimCampus] (
+    [CampusID]         INT            NOT NULL,
+    [TenantID]         INT            NOT NULL,
+    [Code]             NVARCHAR (255) NOT NULL,
+    [Name]             NVARCHAR (255) NOT NULL,
+    [StartDateTime]    DATETIME       NOT NULL,
+    [EndDateTime]      DATETIME       NULL,
+    [Active]           BIT            NOT NULL,
+    [ExecutionID]      VARCHAR (50)   NOT NULL,
+    [InsertedDateTime] DATETIME       DEFAULT (getutcdate()) NULL,
+    [UpdatedDateTime]  DATETIME       DEFAULT (getutcdate()) NULL,
+    [Hashvalue]        NVARCHAR (64)  NOT NULL,
+    CONSTRAINT [pk_DWDimCampus__CampusID] PRIMARY KEY CLUSTERED ([CampusID] ASC),
+    CONSTRAINT [FK_DWDimCampus_Tenant__TenantID] FOREIGN KEY ([TenantID]) REFERENCES [dbo].[Tenant] ([TenantID])
+);
+GO
+
---
+++ Extract/Tables/FellowshipOne_Attendance.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE [Extract].[FellowshipOne_Attendance] (
+    [RowNumber]         INT            NULL,
+    [Assigned]          NVARCHAR (20)  NULL,
+    [ActivityType]      NVARCHAR (100) NULL,
+    [Ministry]          NVARCHAR (100) NULL,
+    [Activity]          NVARCHAR (100) NULL,
+    [RosterFolder]      NVARCHAR (100) NULL,
+    [Roster]            NVARCHAR (100) NULL,
+    [BreakoutGroup]     NVARCHAR (100) NULL,
+    [VolStaffType]      NVARCHAR (100) NULL,
+    [VolStaffJob]       NVARCHAR (100) NULL,
+    [VolStaffSchedule]  NVARCHAR (100) NULL,
+    [IndividualID]      NVARCHAR (100) NULL,
+    [Name]              NVARCHAR (100) NULL,
+    [FirstName]         NVARCHAR (100) NULL,
+    [LastName]          NVARCHAR (100) NULL,
+    [DOB]               NVARCHAR (100) NULL,
+    [MaritalStatus]     NVARCHAR (100) NULL,
+    [Gender]            NVARCHAR (100) NULL,
+    [HouseholdPosition] NVARCHAR (100) NULL,
+    [Status]            NVARCHAR (100) NULL,
+    [SubStatus]         NVARCHAR (100) NULL,
+    [School]            NVARCHAR (100) NULL,
+    [HouseholdID]       NVARCHAR (100) NULL,
+    [HouseholdFullName] NVARCHAR (100) NULL,
+    [Address]           NVARCHAR (100) NULL,
+    [City]              NVARCHAR (100) NULL,
+    [StateProvince]     NVARCHAR (100) NULL,
+    [PostalCode]        NVARCHAR (100) NULL,
+    [CheckInTagCode]    NVARCHAR (100) NULL,
+    [CheckInvPager]     NVARCHAR (100) NULL,
+    [CheckInTime]       NVARCHAR (100) NULL,
+    [CheckInStation]    NVARCHAR (100) NULL,
+    [Instance]          NVARCHAR (100) NULL,
+    [ExecutionID]       VARCHAR (50)   NOT NULL,
+    [InsertedDateTime]  DATETIME       DEFAULT (getutcdate()) NULL
+);
+GO
+
+CREATE CLUSTERED INDEX [CIX_ExtractFellowshipOneAttendance__ExecutionID]
+    ON [Extract].[FellowshipOne_Attendance]([ExecutionID] ASC);
+GO
+
