7664b4a20fa33ff2f9b7218cf082f0b2d553b426
shannonlowder <slowder@52projectsinc.com>
2015-12-26 18:16:54 -0500

    Extract_FellowshipOne_Attendance handles multiple sheets now
    BUG: Still can't move the file to completed after finishing.

--- Transform/Tables/DimCampus.sql
+++ Transform/Tables/DimCampus.sql
@@ -1,20 +1,25 @@
-﻿CREATE TABLE [DW].[DimCampus](
+﻿CREATE TABLE Transform.[DimCampus](
     [CampusID]         INT NOT NULL
   , [TenantID]         INT NOT NULL
   , [Code]             NVARCHAR(255) NOT NULL
   , [Name]             NVARCHAR(255) NOT NULL
   , Zipcode            INT NULL
-  , Latitude		   FLOAT NULL
-  , Longitude		   FLOAT NULL
+  , Latitude           FLOAT NULL
+  , Longitude          FLOAT NULL
   , LocationGeographgy GEOGRAPHY NULL
   , [StartDateTime]    DATETIME NOT NULL
   , [EndDateTime]      DATETIME NULL
   , [Active]           BIT NOT NULL
 
-  , [ExecutionID]      VARCHAR(50) NOT NULL
-  , [InsertedDateTime] DATETIME DEFAULT(GETUTCDATE()) NULL
-  , [UpdatedDateTime]  DATETIME DEFAULT(GETUTCDATE()) NULL
-  , [Hashvalue]        NVARCHAR(64) NOT NULL
+    --ETL Specific Columns
+  , ExecutionID        VARCHAR(50) NOT NULL
+  , ETLActionID        INT NOT NULL
+  , InsertedDateTime   DATETIME DEFAULT(GETUTCDATE())
+  , UpdatedDateTime    DATETIME DEFAULT(GETUTCDATE())
+  , Hashvalue          NVARCHAR(64) NOT NULL
+    
+    --Keys should get enforced at this point.
+
   , CONSTRAINT [pk_DWDimCampus__CampusID] PRIMARY KEY CLUSTERED([CampusID] ASC)
   , CONSTRAINT [FK_DWDimCampus_Tenant__TenantID] FOREIGN KEY([TenantID]) REFERENCES [dbo].[Tenant](
                                                                                     [TenantID])
