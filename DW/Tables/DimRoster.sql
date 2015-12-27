be9c37ad9e118c7c41bfa0e9dc3019336c263fd1
shannonlowder <slowder@52projectsinc.com>
2015-12-27 16:23:23 -0500

    Extract and load DimRoster packages unit test SUCCESS!

---
+++ DW/Tables/DimRoster.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [DW].[DimRoster]
+(
+	 RosterID         INT NOT NULL
+  , TenantID         INT NOT NULL
+  , RosterFolder     NVARCHAR(255)
+  , Roster           NVARCHAR(255)
+  , BreakoutGroup    NVARCHAR(255)
+
+  --ETL Specific Columns
+  , [ExecutionID]      VARCHAR(50) NOT NULL
+  , [InsertedDateTime] DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [UpdatedDateTime]  DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [Hashvalue]        NVARCHAR(64) NOT NULL
+  , CONSTRAINT [pk_DWDimRoster__RosterID_TenantID] 
+	   PRIMARY KEY CLUSTERED(RosterID, TenantID)
+  , CONSTRAINT [FK_DWDimRoster_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID])
+);
---
+++ Transform/Tables/DimRoster.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE Transform.[DimRoster](
+    RosterID         INT NOT NULL
+  , TenantID         INT NOT NULL
+  , RosterFolder     NVARCHAR(255)
+  , Roster           NVARCHAR(255)
+  , BreakoutGroup    NVARCHAR(255)
+
+    --ETL Specific Columns
+  , ExecutionID      VARCHAR(50) NOT NULL
+  , ETLActionID      INT NOT NULL
+  , InsertedDateTime DATETIME DEFAULT(GETUTCDATE())
+  , UpdatedDateTime  DATETIME DEFAULT(GETUTCDATE())
+  , Hashvalue        NVARCHAR(64) NOT NULL
+    
+    --Keys should get enforced at this point.
+
+  , CONSTRAINT [PK_TransformDimRoster__RosterID_TenantID] 
+	   PRIMARY KEY CLUSTERED(RosterID, TenantID)
+  , CONSTRAINT [FK_TransformDimRoster_Tenant__TenantID] 
+  FOREIGN KEY([TenantID]) 
+  REFERENCES [dbo].[Tenant]( [TenantID])
+);
