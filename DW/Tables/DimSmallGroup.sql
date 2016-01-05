c86d998b7f1558359c309520d30cf16d193f4135
shannonlowder <slowder@52projectsinc.com>
2016-01-05 10:11:07 -0500

    Transform DimSmallGroup Packages Pass Unit Testing

---
+++ DW/Tables/DimSmallGroup.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [DW].[DimSmallGroup]
+(
+      SmallGroupID INT NOT NULL
+    , TenantID INT NOT NULL
+    , CampusID INT NULL
+    , GroupType NVARCHAR(255) NULL
+    , GroupName NVARCHAR(255) NOT NULL
+    , ZipCode INT NULL
+    , Latitude FLOAT NULL
+    , Longitude FLOAT NULL
+    , LocationGeography GEOGRAPHY NULL
+    
+
+  --ETL Specific Columns
+  , [ExecutionID]      VARCHAR(50) NOT NULL
+  , [InsertedDateTime] DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [UpdatedDateTime]  DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [Hashvalue]        NVARCHAR(64) NOT NULL
+  , CONSTRAINT [pk_DWDimSmallGroup__SmallGroupID_TenantID] 
+	   PRIMARY KEY CLUSTERED(SmallGroupID, TenantID)
+  , CONSTRAINT [FK_DWDimSmallGroup_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID])
+);
---
+++ Transform/Tables/DimSmallGroup.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE [Transform].[DimSmallGroup]
+(
+      SmallGroupID INT NOT NULL
+    , TenantID INT NOT NULL
+    , CampusID INT NULL
+    , GroupType NVARCHAR(255) NULL
+    , GroupName NVARCHAR(255) NOT NULL
+    , ZipCode INT NULL
+    , Latitude FLOAT NULL
+    , Longitude FLOAT NULL
+    , LocationGeography GEOGRAPHY NULL
+    
+
+    --ETL Specific Columns
+    , ExecutionID        VARCHAR(50) NOT NULL
+    , ETLActionID        INT NOT NULL
+    , InsertedDateTime   DATETIME DEFAULT(GETUTCDATE())
+    , UpdatedDateTime    DATETIME DEFAULT(GETUTCDATE())
+    , Hashvalue          NVARCHAR(64) NOT NULL
+    
+    --Keys should get enforced at this point.
+
+  , CONSTRAINT [PK_TransformDimSmallGroup__SmallGroupID_TenantID] 
+	   PRIMARY KEY CLUSTERED(SmallGroupID, TenantID)
+  , CONSTRAINT [FK_TransformDimSmallGroup_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID])
+);
