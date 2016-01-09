023b0508a8185b618a5b971daa3caf34cb51aff4
shannonlowder <slowder@52projectsinc.com>
2016-01-09 17:28:16 -0500

    FactBudgetRevenue transform and loads, unit tests complete

---
+++ Transform/Tables/FactBudgetRevenue.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [Transform].FactBudgetRevenue
+(
+    BudgetRevenueID       INT NOT NULL
+  , TenantID        INT NOT NULL
+  , BudgetYear      INT NOT NULL
+  , BudgetMonth     TINYINT NOT NULL
+  , EntityID		INT NOT NULL -- for now Hardcoded to HBC, HBF, HCA, WITW in post deploy
+  , FinancialCategoryID INT NOT NULL -- covers all the SCodes except 2
+  , Amount			MONEY NOT NULL
+  
+    --ETL Specific Columns
+  , ExecutionID        VARCHAR(50) NOT NULL
+  , ETLActionID        INT NOT NULL
+  , InsertedDateTime   DATETIME DEFAULT(GETUTCDATE())
+  , UpdatedDateTime    DATETIME DEFAULT(GETUTCDATE())
+  , Hashvalue          NVARCHAR(64) NOT NULL
+    
+    --Keys should get enforced at this point.
+
+  , CONSTRAINT [PK_TransformFactBudgetRevenue__BudgetRevenueID_TenantID] 
+	   PRIMARY KEY CLUSTERED(BudgetRevenueID, TenantID)
+  , CONSTRAINT [FK_TransformFactBudgetRevenue_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID])
+);
