b1883526d143d16d6e91d051a9834442cb486872
shannonlowder <slowder@52projectsinc.com>
2016-01-11 10:30:15 -0500

    Finished the MIP import, unit test successful!

---
+++ DW/Tables/FactBudgetExpense.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [DW].FactBudgetExpense
+(
+    BudgetExpenseID       INT NOT NULL
+  , TenantID           INT NOT NULL
+  , BudgetYear      INT NOT NULL
+  , BudgetMonth     TINYINT NOT NULL
+  , EntityID		INT NOT NULL -- for now Hardcoded to HBC, HBF, HCA, WITW in post deploy
+  , FinancialCategoryID INT NOT NULL -- covers all the SCodes except 2
+  , Amount			MONEY NOT NULL
+
+  --ETL Specific Columns
+  , [ExecutionID]      VARCHAR(50) NOT NULL
+  , [InsertedDateTime] DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [UpdatedDateTime]  DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [Hashvalue]        NVARCHAR(64) NOT NULL
+  , CONSTRAINT [pk_DWFactBudgetExpense__BudgetExpenseID_TenantID] 
+	   PRIMARY KEY CLUSTERED(BudgetExpenseID, TenantID)
+  , CONSTRAINT [FK_DWFactBudgetExpense_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID])
+);
---
+++ Transform/Tables/FactBudgetExpense.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [Transform].FactBudgetExpense
+(
+    BudgetExpenseID       INT NOT NULL
+  , TenantID        INT NOT NULL
+  , BudgetYear      INT NOT NULL
+  , BudgetMonth     TINYINT NOT NULL
+  , EntityID		INT NOT NULL -- for now Hardcoded to HBC, HBF, HCA, WITW in post deploy
+  , FinancialCategoryID INT NOT NULL -- covers all the SCodes except 2
+  , Amount			MONEY NOT NULL
+  
+    --ETL Specific Columns
+  , ExecutionID        VARCHAR(50) NOT NULL
+  , ETLActionID        INT NOT NULL
+  , InsertedDateTime   DATETIME DEFAULT(GETUTCDATE())
+  , UpdatedDateTime    DATETIME DEFAULT(GETUTCDATE())
+  , Hashvalue          NVARCHAR(64) NOT NULL
+    
+    --Keys should get enforced at this point.
+
+  , CONSTRAINT [PK_TransformFactBudgetExpense__BudgetExpenseID_TenantID] 
+	   PRIMARY KEY CLUSTERED(BudgetExpenseID, TenantID)
+  , CONSTRAINT [FK_TransformFactBudgetExpense_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID])
+);
