05344173c434215a13fb6d170dbeed2922581b88
shannonlowder <slowder@52projectsinc.com>
2016-01-15 23:39:37 -0500

    Defining the GLGrouping Codes for reporting...short term.

---
+++ dbo/Tables/GlExpenseGrouping.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[GLExpenseGrouping]
+(
+	  GroupID INT NOT NULL 
+	, GLCode NVARCHAR(10) NOT NULL
+)
+GO 
+CREATE CLUSTERED INDEX CIX_GLExpenseGrouping__GroupID_GLCode
+	ON GLExpenseGrouping(GroupID, GLCode)
