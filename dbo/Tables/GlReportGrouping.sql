efed9d454f8b63ca8e0b6c07676df694206f5e1e
shannonlowder <slowder@52projectsinc.com>
2016-01-17 15:50:06 -0500

    Entities Financial statement report COMPLETE!

---
+++ dbo/Tables/GlReportGrouping.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[GLReportGrouping]
+(
+	  GroupID INT NOT NULL 
+	, GLCode NVARCHAR(10) NOT NULL
+	, Category NVARCHAR(10) NOT NULL -- Revenue or Expense
+)
+GO 
+CREATE CLUSTERED INDEX CIX_GLReportGrouping__GroupID_GLCode
+	ON GLReportGrouping(GroupID, GLCode)
