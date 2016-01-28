003e3753753c836b55db09a8902548e67aaf3def
shannonlowder <slowder@52projectsinc.com>
2016-01-28 12:27:38 -0500

    Completed the HBC LeadIndicators report

--- DW/Tables/Copy of FactStewardshipGiving.sql
+++ DW/Tables/Copy of FactStewardshipGiving.sql
@@ -1,19 +1,19 @@
-﻿CREATE TABLE [DW].FactHBFMetric
+﻿CREATE TABLE [DW].FactStewardshipGiving
 (
-    HBFMetricID INT NOT NULL 
+    StewardshipGivingID INT NOT NULL 
   , TenantID INT NOT NULL
-  , MetricName NVARCHAR(255) NOT NULL
+  , CampusID INT NOT NULL
   , CalendarMonth INT NOT NULL
   , CalendarYear INT NOT NULL
-  , HBFMetricAmount INT NOT NULL
+  , StewardshipGivingAmount INT NOT NULL
   --ETL Specific Columns
   , [ExecutionID]      VARCHAR(50) NOT NULL
   , [InsertedDateTime] DATETIME DEFAULT(GETUTCDATE()) NULL
   , [UpdatedDateTime]  DATETIME DEFAULT(GETUTCDATE()) NULL
   , [Hashvalue]        NVARCHAR(64) NOT NULL
-  , CONSTRAINT [pk_DWFactHBFMetric__HBFMetricID_TenantID] 
-	   PRIMARY KEY CLUSTERED(HBFMetricID, TenantID)
-  , CONSTRAINT [FK_DWFactHBFMetric_Tenant__TenantID] 
+  , CONSTRAINT [pk_DWFactStewardshipGiving__StewardshipGivingID_TenantID] 
+	   PRIMARY KEY CLUSTERED(StewardshipGivingID, TenantID)
+  , CONSTRAINT [FK_DWFactStewardshipGiving_Tenant__TenantID] 
 	   FOREIGN KEY([TenantID]) 
 	   REFERENCES [dbo].[Tenant]( [TenantID])
 );
---
+++ Transform/Tables/FactHBFMetric.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [Transform].[FactHBFMetric]
+(
+	HBFMetricID INT NOT NULL 
+  , TenantID INT NOT NULL
+  , MetricName NVARCHAR(255) NOT NULL
+  , CalendarMonth INT NOT NULL
+  , CalendarYear INT NOT NULL
+  , HBFMetricAmount INT NOT NULL
+	   --ETL Specific Columns
+  , ExecutionID        VARCHAR(50) NOT NULL
+  , ETLActionID        INT NOT NULL
+  , InsertedDateTime   DATETIME DEFAULT(GETUTCDATE())
+  , UpdatedDateTime    DATETIME DEFAULT(GETUTCDATE())
+  , Hashvalue          NVARCHAR(64) NOT NULL
+   
+    --Keys should get enforced at this point.
+
+  , CONSTRAINT [PK_TransformHBFMetric__HBFMetricID_TenantID] 
+	   PRIMARY KEY CLUSTERED(HBFMetricID, TenantID)
+  , CONSTRAINT [FK_TransformFactHBFMetric_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID]) 
+)
