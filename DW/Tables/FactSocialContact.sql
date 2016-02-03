00a982753e877e742025f92592588033278e143b
shannonlowder <slowder@52projectsinc.com>
2016-02-02 21:32:08 -0500

    Finished etl for importing the web form social data.

---
+++ DW/Tables/FactSocialContact.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [DW].FactSocialContact
+(
+    SocialContactID		INT NOT NULL 
+  , TenantID			INT NOT NULL
+  , SocialNetwork		NVARCHAR(255)
+  , CalendarMonth		INT NOT NULL
+  , CalendarYear		INT NOT NULL
+  , SocialContactAmount INT NOT NULL
+
+  --ETL Specific Columns
+  , [ExecutionID]      VARCHAR(50) NOT NULL
+  , [InsertedDateTime] DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [UpdatedDateTime]  DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [Hashvalue]        NVARCHAR(64) NOT NULL
+  , CONSTRAINT [pk_DWFactSocialContact__SocialContactID_TenantID] 
+	   PRIMARY KEY CLUSTERED(SocialContactID, TenantID)
+  , CONSTRAINT [FK_DWFactSocialContact_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID])
+);
---
+++ Transform/Tables/FactSocialContact.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [Transform].[FactSocialContact]
+(
+	SocialContactID		INT NOT NULL 
+  , TenantID			INT NOT NULL
+  , SocialNetwork		NVARCHAR(255)
+  , CalendarMonth		INT NOT NULL
+  , CalendarYear		INT NOT NULL
+  , SocialContactAmount INT NOT NULL
+	   --ETL Specific Columns
+  , ExecutionID        VARCHAR(50) NOT NULL
+  , ETLActionID        INT NOT NULL
+  , InsertedDateTime   DATETIME DEFAULT(GETUTCDATE())
+  , UpdatedDateTime    DATETIME DEFAULT(GETUTCDATE())
+  , Hashvalue          NVARCHAR(64) NOT NULL
+   
+    --Keys should get enforced at this point.
+
+  , CONSTRAINT [PK_TransformSocialContact__SocialContactID_TenantID] 
+	   PRIMARY KEY CLUSTERED(SocialContactID, TenantID)
+  , CONSTRAINT [FK_TransformFactSocialContact_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID]) 
+)
+
+
