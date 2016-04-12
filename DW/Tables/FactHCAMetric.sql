872eb1b79a5cc6607c317ae1b186ebf40914afab
shannonlowder <slowder@52projectsinc.com>
2016-04-11 20:29:26 -0400

    New HCA tables

---
+++ DW/Tables/FactHCAMetric.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [DW].FactHCAMetric
+(
+    HCAMetricID INT NOT NULL 
+  , TenantID INT NOT NULL
+  , SectionName NVARCHAR(255) NULL
+  , MetricName NVARCHAR(255) NOT NULL
+  , CalendarMonth INT NOT NULL
+  , CalendarYear INT NOT NULL
+  , HCAMetricAmount INT NOT NULL
+  --ETL Specific Columns
+  , [ExecutionID]      VARCHAR(50) NOT NULL
+  , [InsertedDateTime] DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [UpdatedDateTime]  DATETIME DEFAULT(GETUTCDATE()) NULL
+  , [Hashvalue]        NVARCHAR(64) NOT NULL
+  , CONSTRAINT [pk_DWFactHCAMetric__HCAMetricID_TenantID] 
+	   PRIMARY KEY CLUSTERED(HCAMetricID, TenantID)
+  , CONSTRAINT [FK_DWFactHCAMetric_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID])
+);
---
+++ Transform/Tables/FactHCAMetric.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [Transform].[FactHCAMetric]
+(
+	HCAMetricID INT NOT NULL 
+  , TenantID INT NOT NULL
+  , SectionName NVARCHAR(255) NULL
+  , MetricName NVARCHAR(255) NOT NULL
+  , CalendarMonth INT NOT NULL
+  , CalendarYear INT NOT NULL
+  , HCAMetricAmount INT NOT NULL
+	   --ETL Specific Columns
+  , ExecutionID        VARCHAR(50) NOT NULL
+  , ETLActionID        INT NOT NULL
+  , InsertedDateTime   DATETIME DEFAULT(GETUTCDATE())
+  , UpdatedDateTime    DATETIME DEFAULT(GETUTCDATE())
+  , Hashvalue          NVARCHAR(64) NOT NULL
+   
+    --Keys should get enforced at this point.
+
+  , CONSTRAINT [PK_TransformHCAMetric__HCAMetricID_TenantID] 
+	   PRIMARY KEY CLUSTERED(HCAMetricID, TenantID)
+  , CONSTRAINT [FK_TransformFactHCAMetric_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID]) 
+)
