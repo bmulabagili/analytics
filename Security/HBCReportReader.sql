7a62c178709b7180af19b2688f1892dc0d6ae89a
shannonlowder <slowder@52projectsinc.com>
2016-07-31 13:27:15 -0400

    Had to change the file type of hbcreportreader user

--- Security/HBCReportReader.sql
+++ Security/HBCReportReader.sql
@@ -1,8 +1,8 @@
-﻿--master
-CREATE LOGIN [HBCReportReader]
-
---AnalytyicsDB
-
+﻿USE [master]
+CREATE LOGIN [HBCReportReader] WITH PASSWORD = 'R3p0rts*'
+GO
+USE AnalytyicsDB
+GO
 CREATE USER [HBCReportReader]
 GO
 GRANT CONNECT TO [HBCReportReader]
