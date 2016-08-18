7b3b4f13d3260b36f02161cb5e4dbd053e2bfdfc
shannonlowder <slowder@52projectsinc.com>
2016-08-18 16:10:53 -0400

    Added WITW fact tables, transform and load packages.

---
+++ DW/Tables/FactWITWMetric.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [DW].FactWITWMetric
+(
+    WITWMetricID INT NOT NULL 
+  , TenantID INT NOT NULL
+  , SectionName NVARCHAR(255) NULL
+  , MetricName NVARCHAR(255) NOT NULL
+  , CalendarMonth INT NOT NULL
+  , CalendarYear INT NOT NULL
+  , WITWMetricAmount INT NOT NULL
+  --ETL Specific Columns
+  , [ExecutionID]      VARCHAR(50) NOT NULL
+  , [InsertedDateTime] DATETIME DEFAULT(GETDATE()) NULL
+  , [UpdatedDateTime]  DATETIME DEFAULT(GETDATE()) NULL
+  , [Hashvalue]        NVARCHAR(64) NOT NULL
+  , CONSTRAINT [pk_DWFactWITWMetric__WITWMetricID_TenantID] 
+	   PRIMARY KEY CLUSTERED(WITWMetricID, TenantID)
+  , CONSTRAINT [FK_DWFactWITWMetric_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID])
+);
---
+++ Transform/Tables/FactWITWMetric.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [Transform].[FactWITWMetric]
+(
+	WITWMetricID INT NOT NULL 
+  , TenantID INT NOT NULL
+  , SectionName NVARCHAR(255) NULL
+  , MetricName NVARCHAR(255) NOT NULL
+  , CalendarMonth INT NOT NULL
+  , CalendarYear INT NOT NULL
+  , WITWMetricAmount INT NOT NULL
+	   --ETL Specific Columns
+  , ExecutionID        VARCHAR(50) NOT NULL
+  , ETLActionID        INT NOT NULL
+  , InsertedDateTime   DATETIME DEFAULT(GETDATE())
+  , UpdatedDateTime    DATETIME DEFAULT(GETDATE())
+  , Hashvalue          NVARCHAR(64) NOT NULL
+   
+    --Keys should get enforced at this point.
+
+  , CONSTRAINT [PK_TransformWITWMetric__WITWMetricID_TenantID] 
+	   PRIMARY KEY CLUSTERED(WITWMetricID, TenantID)
+  , CONSTRAINT [FK_TransformFactWITWMetric_Tenant__TenantID] 
+	   FOREIGN KEY([TenantID]) 
+	   REFERENCES [dbo].[Tenant]( [TenantID]) 
+)
